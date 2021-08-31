290fe64316efab51bb3c4720f1fd5ca139ecac33
sahar <saharlaila@gmail.com>
2021-08-31 19:16:58 +0300

    Sahar Push

--- src/components/itinerary/static/time.line/TimelineView.js
+++ src/components/itinerary/static/time.line/TimelineView.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import 'react-vertical-timeline-component/style.min.css';
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
-import {Col, Row} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 
 import {
     BiHotel,
@@ -28,113 +28,119 @@ import {Bus, Car, Walk} from "tabler-icons-react";
 
 const TimelineView = (props) => {
 
-        const itineraryDays = props.itinerary.itineraryDays;
-        const [currentDay, setCurrentDay] = useState(itineraryDays[0]);
+    const itineraryDays = props.itinerary.itineraryDays;
+    const [currentDay, setCurrentDay] = useState(itineraryDays[0]);
+    console.log(currentDay);
 
-        const getDates = () => {
-            const formatDate = (date) => {
-                if (date !== undefined) {
-                    return date.year + '-' + date.month + '-' + date.day;
-                }
+    const getDates = () => {
+        const formatDate = (date) => {
+            if (date !== undefined) {
+                return date.year + '-' + date.month + '-' + date.day;
             }
-
-            return itineraryDays.map(day => formatDate(day.date))
         }
 
-        let dayChangedHandler = (index) => {
-            setCurrentDay(itineraryDays[index]);
+        return itineraryDays.map(day => formatDate(day.date))
+    }
+
+    let dayChangedHandler = (index) => {
+        setCurrentDay(itineraryDays[index]);
+    };
+
+    let getAttractionIcon = (type) => {
+        // icons from https://react-icons.github.io/react-icons/search?q=zoo
+        type = type.replaceAll(' ', '');
+
+        let map = {
+            Bar: <IoBeer/>,
+            Restaurant: <GiMeal/>,
+            Aquarium: <GiAquarium/>,
+            Cafe: <IoCafeOutline/>,
+            Beach: <FaUmbrellaBeach/>,
+            Casino: <GiPokerHand/>,
+            ArtGallery: <RiGalleryLine/>,
+            Hotel: <BiHotel/>,
+            Market: <FiShoppingCart/>,
+            Museum: <GiMonaLisa/>,
+            NightClub: <GiMartini/>,
+            ShoppingMall: <GiShoppingCart/>,
+            TopSights: <GiBinoculars/>,
+            TouristAttraction: <FaMapMarkedAlt/>,
+            Zoo: <MdPets/>,
+            AmusementPark: <RiMickeyLine/>,
+            Park: <TiTree/>,
+            Spa: <MdSpa/>
         };
 
-        let getAttractionIcon = (type) => {
-            // icons from https://react-icons.github.io/react-icons/search?q=zoo
-            type = type.replaceAll(' ', '');
+        return map[type] !== undefined ? map[type] : <FaRegLaughBeam/>;
+    }
 
-            let map = {
-                Bar: <IoBeer/>,
-                Restaurant: <GiMeal/>,
-                Aquarium: <GiAquarium/>,
-                Cafe: <IoCafeOutline/>,
-                Beach: <FaUmbrellaBeach/>,
-                Casino: <GiPokerHand/>,
-                ArtGallery: <RiGalleryLine/>,
-                Hotel: <BiHotel/>,
-                Market: <FiShoppingCart/>,
-                Museum: <GiMonaLisa/>,
-                NightClub: <GiMartini/>,
-                ShoppingMall: <GiShoppingCart/>,
-                TopSights: <GiBinoculars/>,
-                TouristAttraction: <FaMapMarkedAlt/>,
-                Zoo: <MdPets/>,
-                AmusementPark: <RiMickeyLine/>,
-                Park: <TiTree/>,
-                Spa: <MdSpa/>
-            };
 
-            return map[type] !== undefined ? map[type] : <FaRegLaughBeam/>;
+    function getTransportDuration(activity) {
+        let res = "";
+        const startTime = Date.parse('December 17, 1995 ' + activity.startTime + ":00");
+        const endTime = Date.parse('December 17, 1995 ' + activity.endTime + ":00");
+        const duration = endTime - startTime;
+
+        return new Date(duration).getMinutes() + " minutes";
+    }
+
+    const mapAttraction = (activity, index) => {
+        let innerComponent;
+        let icon;
+        let isTransport = false;
+
+        if (activity.type === "ATTRACTION") {
+            innerComponent = <AttractionTimeline attraction={activity.attraction}/>
+            icon = getAttractionIcon(activity.attraction.type)
+        } else if (activity.type === "TRANSIT") {
+            isTransport = true;
+            icon = <Bus color={"white"}/>
+        } else if (activity.type === "CAR") {
+            isTransport = true;
+            icon = <Car color={"white"}/>
+        } else if (activity.type === "WALK") {
+            isTransport = true;
+            icon = <Walk color={"white"}/>
+        } else if (activity.type === "FREE_TIME") {
+            return;
         }
-
-
-        function getTransportDuration(activity) {
-            let res = "";
-            const startTime = Date.parse('December 17, 1995 ' + activity.startTime + ":00");
-            const endTime = Date.parse('December 17, 1995 ' + activity.endTime + ":00");
-            const duration = endTime - startTime;
-
-            return new Date(duration).getMinutes() + " minutes";
-        }
-
-        const mapAttraction = (activity, index) => {
-            let innerComponent;
-            let icon;
-            let isTransport = false;
-
-            if (activity.type === "ATTRACTION") {
-                innerComponent = <AttractionTimeline attraction={activity.attraction}/>
-                icon = getAttractionIcon(activity.attraction.type)
-            } else if (activity.type === "TRANSIT") {
-                isTransport = true;
-                icon = <Bus color={"white"}/>
-            } else if (activity.type === "CAR") {
-                isTransport = true;
-                icon = <Car color={"white"}/>
-            } else if (activity.type === "WALK") {
-                isTransport = true;
-                icon = <Walk color={"white"}/>
-            } else if (activity.type === "FREE_TIME") {
-                return;
-            }
-
-            return (
-                <VerticalTimelineElement
-                    key={index}
-                    className={isTransport ? "" : "vertical-timeline-element--work"}
-                    date={isTransport ? getTransportDuration(activity) : activity.startTime + " - " + activity.endTime}
-                    iconStyle={{background: '#467fcf', color: '#fff'}}
-                    icon={icon}
-                >
-                    {innerComponent}
-                </VerticalTimelineElement>
-            )
-        };
-
 
         return (
-            <>
-                <Row>
-                    <Col/>
-                    <Col md={10}>
-                        <DayPicker onDayChange={dayChangedHandler} dates={getDates()}/>
-                    </Col>
-                    <Col/>
-                </Row>
-                <Row>
-                    <VerticalTimeline>
-                        {currentDay.activities.map((activity, index) => mapAttraction(activity, index))}
-                    </VerticalTimeline>
-                </Row>
-            </>
-        );
-    }
+            <VerticalTimelineElement
+                key={index}
+                className={isTransport ? "" : "vertical-timeline-element--work"}
+                date={isTransport ? getTransportDuration(activity) : activity.startTime + " - " + activity.endTime}
+                iconStyle={{background: '#467fcf', color: '#fff'}}
+                icon={icon}
+            >
+                {innerComponent}
+            </VerticalTimelineElement>
+        )
+    };
+
+    return (
+        <>
+            <Row>
+                <Col/>
+                <Col md={10}>
+                    <DayPicker onDayChange={dayChangedHandler} dates={getDates()}/>
+                </Col>
+                <Col/>
+            </Row>
+            <Row>
+                {currentDay.activities.length > 1 && <VerticalTimeline>
+                    {currentDay.activities.map((activity, index) => mapAttraction(activity, index))}
+                </VerticalTimeline>}
+                {
+                    currentDay.activities.length === 1 &&
+                    <div style={{textAlign: "center"}}>
+                        <h3>No Activities</h3>
+                    </div>
+                }
+            </Row>
+        </>
+    );
+}
 ;
 
 export default TimelineView;
--- src/pages/myItineraries/SingleItinerary.js
+++ src/pages/myItineraries/SingleItinerary.js
@@ -16,9 +16,9 @@ import {
     GiPokerHand,
     GiShoppingCart,
     IoBeer,
-    IoCafeOutline,
+    IoCafeOutline, MdFlightLand, MdFlightTakeoff,
     MdPets,
-    MdSpa,
+    MdSpa, RiFlightTakeoffLine,
     RiGalleryLine,
     RiMickeyLine,
     TiTree
@@ -26,67 +26,70 @@ import {
 import Tooltip from "react-bootstrap/Tooltip";
 import Button from "react-bootstrap/Button";
 import * as Constants from "../../components/itinerary/Constants";
+import {Trash} from "tabler-icons-react";
+import styles from "./singleItinerary.moudule.css"
 
 const SingleItinerary = (props) => {
 
 
-    let fromDateToString = (date) => {
-        let mm = date.month + 1;
-        let dd = date.day;
+        let fromDateToString = (date) => {
+            let mm = date.month + 1;
+            let dd = date.day;
 
-        return ((dd > 9 ? '' : '0') + dd) + '.' + ((mm > 9 ? '' : '0') + mm) + '.' + date.year;
-    }
+            return ((dd > 9 ? '' : '0') + dd) + '.' + ((mm > 9 ? '' : '0') + mm) + '.' + date.year;
+        }
 
-    const questionnaire = props.questionsData;
-    const country = questionnaire.country;
-    const city = questionnaire.city;
-    const adultsCount = questionnaire.adultsCount;
-    const childrenCount = questionnaire.childrenCount;
-    const startDate = fromDateToString(questionnaire.startDate.date);
-    const endDate = fromDateToString(questionnaire.endDate.date);
-    const tripVibes = questionnaire.tripVibes; //TODO -> What i should do with this information ?
-    const myIndex = props.index;
-    const favoriteAttractions = questionnaire.favoriteAttractions.map(attraction => attraction.tagName.replaceAll(" ", "") + " ");
-    const status = props.status;
-    const itineraryId = props.itiniraryId;
+        const questionnaire = props.questionsData;
+        const flight = questionnaire.flight;
+        const country = questionnaire.country;
+        const city = questionnaire.city;
+        const adultsCount = questionnaire.adultsCount;
+        const childrenCount = questionnaire.childrenCount;
+        const startDate = fromDateToString(questionnaire.startDate.date);
+        const endDate = fromDateToString(questionnaire.endDate.date);
+        const tripVibes = questionnaire.tripVibes; //TODO -> What i should do with this information ?
+        const myIndex = props.index;
+        const favoriteAttractions = questionnaire.favoriteAttractions.map(attraction => attraction.tagName.replaceAll(" ", "") + " ");
+        const status = props.status;
+        const itineraryId = props.itiniraryId;
 
 
-    const renderTooltip = (props, text) => (
-        <Tooltip id="tooltip" {...props}>
-            {text}
-        </Tooltip>
-    );
+        const renderTooltip = (props, text) => (
+            <Tooltip id="tooltip" {...props}>
+                {text}
+            </Tooltip>
+        );
 
-    let getAttractionIcon = (type) => {
-        // icons from https://react-icons.github.io/react-icons/search?q=zoo
-        const typeWithoutSpace = type.replaceAll(' ', '');
+        let getAttractionIcon = (type) => {
+            // icons from https://react-icons.github.io/react-icons/search?q=zoo
+            const typeWithoutSpace = type.replaceAll(' ', '');
 
-        let map = {
-            Bar: <IoBeer/>,
-            Restaurant: <GiMeal/>,
-            Aquarium: <GiAquarium/>,
-            Cafe: <IoCafeOutline/>,
-            Beach: <FaUmbrellaBeach/>,
-            Casino: <GiPokerHand/>,
-            ArtGallery: <RiGalleryLine/>,
-            Hotel: <BiHotel/>,
-            Market: <FiShoppingCart/>,
-            Museum: <GiMonaLisa/>,
-            NightClub: <GiMartini/>,
-            ShoppingMall: <GiShoppingCart/>,
-            TopSights: <GiBinoculars/>,
-            TouristAttraction: <FaMapMarkedAlt/>,
-            Zoo: <MdPets/>,
-            AmusementPark: <RiMickeyLine/>,
-            Park: <TiTree/>,
-            Spa: <MdSpa/>
-        };
+            let map = {
+                Bar: <IoBeer/>,
+                Restaurant: <GiMeal/>,
+                Aquarium: <GiAquarium/>,
+                Cafe: <IoCafeOutline/>,
+                Beach: <FaUmbrellaBeach/>,
+                Casino: <GiPokerHand/>,
+                ArtGallery: <RiGalleryLine/>,
+                Hotel: <BiHotel/>,
+                Market: <FiShoppingCart/>,
+                Museum: <GiMonaLisa/>,
+                NightClub: <GiMartini/>,
+                ShoppingMall: <GiShoppingCart/>,
+                TopSights: <GiBinoculars/>,
+                TouristAttraction: <FaMapMarkedAlt/>,
+                Zoo: <MdPets/>,
+                AmusementPark: <RiMickeyLine/>,
+                Park: <TiTree/>,
+                Spa: <MdSpa/>
+            };
 
-        if (map[typeWithoutSpace] === undefined) {
-            return undefined;
-        } else {
-            return (
-                <span style={{marginRight: "10px"}}>
+            if (map[typeWithoutSpace] === undefined) {
+                return undefined;
+            } else {
+                return (
+                    <span style={{marginRight: "10px"}}>
                 <OverlayTrigger
                     placement="bottom"
                     delay={{show: 250, hide: 400}}
@@ -97,132 +100,187 @@ const SingleItinerary = (props) => {
                     </span>
                 </OverlayTrigger>
                 </span>
-            )
-        }
-    }
-
-    let getAttractionsIcons = () => {
-        let res = [];
-
-        favoriteAttractions.forEach(type => {
-            const icon = getAttractionIcon(type)
-            if (icon !== undefined) {
-                res.push(icon);
+                )
             }
-        })
-
-        return res
-    }
-
-
-    function CheckoutBtnClickEventHandler(e) {
-        localStorage.setItem(Constants.ITINERARY_ID_STORAGE, itineraryId);
-        window.location = Constants.STATIC_VIEW_URL;
-    }
-
-    function editBtnClickEventHandler(e) {
-        localStorage.setItem(Constants.ITINERARY_ID_STORAGE, itineraryId);
-        window.location = Constants.DND_URL;
-    }
-
-    function getStatus() {
-        let res;
-
-        if (status === "EDIT") {
-            res = <span style={{fontWeight: 'bold', color: '#EED202'}}>Not Completed</span>
-
-        } else if (status === "COMPLETED") {
-            res = <span style={{fontWeight: 'bold', color: '#49ee02'}}>Completed</span>
-        } else if (status === "AUTO_MODE") {
-            res = <span style={{fontWeight: 'bold', color: '#ee021e'}}>Processing</span>
         }
 
-        return res;
-    }
+        let getAttractionsIcons = () => {
+            let res = [];
 
-    return (
-        <Card style={{width: '25rem', marginRight: "0", marginBottom: "0"}}>
-            <Card.Header>{startDate} - {endDate}</Card.Header>
-            <Card.Body>
-                <Card.Title>{city}, {country}</Card.Title>
-                <Card.Text>
+            favoriteAttractions.forEach(type => {
+                const icon = getAttractionIcon(type)
+                if (icon !== undefined) {
+                    res.push(icon);
+                }
+            })
+
+            return res.length !== 0 ? res : "None";
+        }
+
+
+        function CheckoutBtnClickEventHandler(e) {
+            localStorage.setItem(Constants.ITINERARY_ID_STORAGE, itineraryId);
+            window.location = Constants.STATIC_VIEW_URL;
+        }
+
+        function editBtnClickEventHandler(e) {
+            localStorage.setItem(Constants.ITINERARY_ID_STORAGE, itineraryId);
+            window.location = Constants.DND_URL;
+        }
+
+        function getStatus() {
+            let res;
+
+            if (status === "EDIT") {
+                res = <span style={{fontWeight: 'bold', color: '#EED202'}}>Not Completed</span>
+
+            } else if (status === "COMPLETED") {
+                res = <span style={{fontWeight: 'bold', color: '#49ee02'}}>Completed</span>
+            } else if (status === "AUTO_MODE") {
+                res = <span style={{fontWeight: 'bold', color: '#ee021e'}}>Processing</span>
+            }
+
+            return res;
+        }
+
+        function onRemoveHandler() {
+            alert("remove : " + itineraryId)
+        }
+
+        return (
+            <Card style={{width: '25rem', marginRight: "0", marginBottom: "0"}}>
+                <Card.Header style={{display: "inline"}}>
                     <Row>
-                        <Col>
-                            <Row>
-                                <Col>
-                                    <OverlayTrigger
-                                        placement="top"
-                                        delay={{show: 250, hide: 400}}
-                                        overlay={(props) => renderTooltip(props, "Adults")}
-                                    >
+                        <Col md={10}>
+                            {startDate} - {endDate}
+                        </Col>
+                        <Col md={2}>
+                            <Button
+                                onClick={onRemoveHandler}
+                                style={{backgroundColor: "transparent", border: "none"}}
+                                size={"sm"}>
+                                <Trash color={"black"} strokeWidth={2}/>
+                            </Button>
+                        </Col>
+                    </Row>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>{city}, {country}</Card.Title>
+                    <Card.Text>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <OverlayTrigger
+                                            placement="top"
+                                            delay={{show: 250, hide: 400}}
+                                            overlay={(props) => renderTooltip(props, "Adults")}
+                                        >
                                         <span>
                                         <FaMale/>
                                         <FaFemale/>
                                         </span>
-                                    </OverlayTrigger>
-                                </Col>
-                                <Col>
-                                    <div style={{marginTop: "3px"}}> {adultsCount}</div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Row>
-                                <Col>
-                                    <OverlayTrigger
-                                        placement="bottom"
-                                        delay={{show: 250, hide: 400}}
-                                        overlay={(props) => renderTooltip(props, "Children")}
-                                    >
+                                        </OverlayTrigger>
+                                    </Col>
+                                    <Col>
+                                        <div style={{marginTop: "3px"}}> {adultsCount}</div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <OverlayTrigger
+                                            placement="bottom"
+                                            delay={{show: 250, hide: 400}}
+                                            overlay={(props) => renderTooltip(props, "Children")}
+                                        >
                                         <span>
                                             <FaChild/>
                                         </span>
-                                    </OverlayTrigger>
-                                </Col>
-                                <Col>
-                                    <div style={{marginTop: "3px"}}> {childrenCount}</div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
+                                        </OverlayTrigger>
+                                    </Col>
+                                    <Col>
+                                        <div style={{marginTop: "3px"}}> {childrenCount}</div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                Status: {getStatus()}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                {/*<Card.Text className={"text-muted"}>*/}
+                                <Card.Text>
+                                    Favorites Attractions: {getAttractionsIcons()}
+                                    {/*{favoriteAttractions}*/}
+                                </Card.Text>
+                            </Col>
+                        </Row>
+                        {flight && <Row>
+                            <Col md={2}>
+                                <OverlayTrigger
+                                    placement="bottom"
+                                    delay={{show: 250, hide: 400}}
+                                    overlay={(props) => renderTooltip(props, "Outbound")}
+                                >
+                                        <span>
+                                            <RiFlightTakeoffLine/>
+                                        </span>
+                                </OverlayTrigger>
+                            </Col>
+                            <Col>
+                                <div
+                                    style={{marginTop: "3px"}}> {"From: " + flight.Outbound.departureAirport + ", Arrival Time: " + flight.Outbound.arrivalTime}
+                                </div>
+                            </Col>
+                        </Row>}
+                        {flight && <Row>
+                            <Col md={2}>
+                                <OverlayTrigger
+                                    placement="bottom"
+                                    delay={{show: 250, hide: 400}}
+                                    overlay={(props) => renderTooltip(props, "Return")}
+                                >
+                                        <span>
+                                            <MdFlightLand/>
+                                        </span>
+                                </OverlayTrigger>
+                            </Col>
+                            <Col>
+                                <div
+                                    style={{marginTop: "3px"}}> {"From: " + flight.Return.departureAirport + ", Arrival Time: " + flight.Return.arrivalTime}
+                                </div>
+                            </Col>
+                        </Row>}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
                     <Row>
                         <Col>
-                            Status: {getStatus()}
+                            <div className="d-grid gap-2">
+                                <Button onClick={CheckoutBtnClickEventHandler}>
+                                    View
+                                </Button>
+                            </div>
                         </Col>
-                    </Row>
-                    <Row>
                         <Col>
-                            {/*<Card.Text className={"text-muted"}>*/}
-                            <Card.Text>
-                                Favorites Attractions: {getAttractionsIcons()}
-                                {/*{favoriteAttractions}*/}
-                            </Card.Text>
+                            <div className="d-grid gap-2">
+                                <Button onClick={editBtnClickEventHandler}>
+                                    Edit
+                                </Button>
+                            </div>
                         </Col>
                     </Row>
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <Row>
-                    <Col>
-                        <div className="d-grid gap-2">
-                            <Button onClick={CheckoutBtnClickEventHandler}>
-                                View
-                            </Button>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="d-grid gap-2">
-                            <Button onClick={editBtnClickEventHandler}>
-                                Edit
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card.Footer>
-        </Card>
-    );
-};
+                </Card.Footer>
+            </Card>
+        );
+    }
+;
 
 export default SingleItinerary;
